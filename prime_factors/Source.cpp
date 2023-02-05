d0c9947a4b0badbef989d03019fc8ab2dfe68c73
NaeimMoafi <naeimcm@gmail.com>
2023-02-05 20:35:52 +0100

    update prime_factors

--- prime_factors/Source.cpp
+++ prime_factors/Source.cpp
@@ -1,47 +1,72 @@
 #include <algorithm>
-#include <format>
 #include <iostream>
 #include <iterator>
 #include <vector>
 
-using ull = unsigned long long;
-using v_ull = std::vector<ull>;
 
-v_ull prime_factors(ull n)
+/*
+prime factors of a given number
+ 
+2 --> while num % 2 == 0; n = n/2;
+
++++++++
+3 --> while num%3 == 0; n = n/3
+
+5
+
+7
+
+** 9 already checked by 3
+
+11
+
+13
+
+** already cheched by 3 and 5 
+
+17 .....
+++++++ --> we can start from i = 3 and i += 2 till we reach the sqrt(num)
+
+check if n is greater that the smallest prime number 2 if yes add it as final factor
+*/
+
+
+std::vector<unsigned long long> find_prime_factors(unsigned long long num)
 {
-	v_ull v;
-	while (n % 2 == 0)
-	{
-		v.push_back(2);
-		n = n / 2;
-	}
-	for (ull i{ 3 }; i * i <= n; i+=2)
-	{
-		while (n % i == 0)
-		{
-			v.push_back(i);
-			n = n / i;
-		}
-	}
+    std::vector<unsigned long long> v_factors;
 
-	if (n > 2)
-	{
-		v.push_back(n);
-	}
+    while(num % 2 == 0)
+    {
+        v_factors.push_back(2);
+        num /= 2;
+    }
 
-	return v;
+    for(unsigned long long i {3}; i * i < num; i += 2)
+    {
+        while(num % i == 0)
+        {
+            v_factors.push_back(i);
+            num /= i;
+        }
+    }
+
+    if(num > 2)
+    {
+        v_factors.push_back(num);
+    }
+
+    //sorting the v_factors
+    std::sort(v_factors.begin(), v_factors.end());
+
+    return v_factors;
 }
-
 
 int main()
 {
-	std::cout << "number: ";
-	ull number;
-	std::cin >> number;
-	auto v = prime_factors(number);
-	std::cout << std::format("{} = ", number);
-	std::copy(v.begin(), v.end() - 1, std::ostream_iterator<ull>(std::cout, "x"));
-	std::cout <<*(v.end()-1) << std::endl;
 
-	return EXIT_SUCCESS;
+    auto ans = find_prime_factors(165);
+    std::copy(ans.begin(), ans.end(), std::ostream_iterator<unsigned long long>(std::cout, " "));
+    std::cout << '\n';
+
+    return 0;
 }
